78abefae3fafd1b48f460cf0508b58bcd0113f28
Payal2693 <147045276+Payal2693@users.noreply.github.com>
2026-09-05 23:00:08 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1748,13 +1748,12 @@ if __name__ == "__main__":
 
     print("========================================\n")
 
+import os
+app.run(
+    debug=False,
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000))
+)   
 
-    app.run(
 
-        debug=True,
-
-        host="127.0.0.1",
-
-        port=5000
-
-    )
+ 
